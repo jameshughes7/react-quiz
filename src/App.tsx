65a46e02d4f62d8af05f16f4d8cbcd852b712bf7
jameshughes7 <hugjimbo77@gmail.com>
2020-07-17 23:51:25 +0100

    create react app initiated and App.tsx setup with initial logic

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import QuestionCard from './components/QuestionCard';
+
+const App = () => {
+  const startQuiz = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
+
+  return (
+    <div className="App">
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startQuiz}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading questions</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+}
+
+export default App;
