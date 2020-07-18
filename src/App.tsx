a7f11cd535cd4a35a96f2f4d00b0de412b6393d7
jameshughes7 <hugjimbo77@gmail.com>
2020-07-18 01:40:57 +0100

    shuffleArray added and AnswerObject type added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
 import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
+
 //Components
 import QuestionCard from './components/QuestionCard';
+//Types
+import { QuestionsState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string,
+  answer: string,
+  correct: boolean,
+  correctAnswer: string,
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionsState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  const startQuiz = async () => {
+  console.log(questions);
 
+  const startQuiz = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    )
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -33,14 +58,14 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading questions</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNr={ number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
